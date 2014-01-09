44bb3b9c82f0506e15a3b225c958088e7b6ebd21
Tim Gibbon <tng@chegwin.org>
2014-01-09 14:35:24 +0000

    Made easier for reading

--- gravity.py
+++ gravity.py
@@ -2,6 +2,10 @@
 # A pound sign (hash) # is used as a comment so that Python ignores it
 # The top line tells the computer what language we are using #!/usr/bin/python
 
+#####YOU NEED TO EDIT THIS VALUE###############################################
+height = 0.70  # height in metres of the electromagnet above the tin foil switch
+
+#######YOU DON'T NEED TO EDIT ANYTHING BELOW HERE (but you can, it's fun!)#####
 ############################IGNORE THESE BITS##################################
 ##### We tell it what external libraries we will be using.
 # In our case, we are using the PiFace###########
@@ -13,12 +17,16 @@ pifacedigital = pifacedigitalio.PiFaceDigital()
 ################################################ End of PiFace additions
 # We are going to need to use the time library
 import time
+#################################################End of imported libraries#####
 
-# Check tinfoil switch is not already pressed
+# Check tinfoil switch is not already pressed and exit if it is
 if ( pifacedigital.input_pins[0].value == 1 ):
     print ("Warning. Tin Foil switch is pressed")
-    print ("I'm out of here.")
+    print ("Exiting.")
     exit()
+################################################################
+
+####################COIL ENERGISING#############################
 input = "n" # We don't want the countdown to start just yet
                     # so we set a variable start_the_program to n
 print "Energising the coils."
@@ -27,6 +35,10 @@ while (input != "y"): # While input not equal to y
     input = raw_input("Press y and hit return to Energise the coil.\n")
 pifacedigital.relays[0].turn_on()
 print ("Coil energised")
+################################################################
+
+
+#################### COUNTDOWN #################################
 input = "n"
 while (input != "y"): # While input not equal to y
     input = raw_input("Press y and hit return to begin the countdown.\n")
@@ -41,7 +53,9 @@ time.sleep(1);
 print ("1");
 time.sleep(1);
 print ("freefall");
+################################################################
 
+################## DROP THE BALL ###############################
 # Switch off the relay and drop the ball bearing
 pifacedigital.relays[0].turn_off()
 
@@ -51,9 +65,11 @@ start_time = time.time()
 while (pifacedigital.input_pins[0].value == 0): # While switch 0 is not closed
     time.sleep(0.0001) # sleep for 10,000th of a second
 
+#Ball has landed at this point
 # Measure the time that switch 0 closed
 end_time = time.time()
 
+# Calculate elapsed time
 elapsed_time = end_time - start_time
 #  %2.6f is a non-integer number with 2 figures before, 6 after decimal point
 print("Elapsed time was %2.6f seconds" % (elapsed_time))
@@ -63,8 +79,6 @@ pifacedigital.relays[0].turn_off()
 
 
 ######## MATHS BIT ############################################################
-height = 0.70  # height in metres of the electromagnet above the tin foil switch
-height = float(height)
 # Distance travelled (height) = initial velocity * time + 0.5 (acceleration * time squared)
 # s = ut + 0.5*g*(t ^ 2)
 # s = 0 + 0.5 gt^2
